f45da7f26bac5151bd175d7021d9e51a5420b573
Faris Halteh <Faris.halteh@gmail.com>
2023-11-19 15:00:00 +0100

    exercise-2: add custom button component

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { View, Text, Image, StyleSheet, SafeAreaView } from "react-native";
-import { Button } from "react-native-elements";
+import CustomButton, { ButtonType } from "../components/CustomButton";
 
 export default function WelcomeScreen() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.topContainer}>
-        <Image
-          style={styles.image}
-          source={{ uri: "https://i.ibb.co/BNwMmVn/img.png" }}
-          resizeMode="contain"
-        />
-        <Text style={styles.text}>DIS students</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button title="Create a free account" />
-        <Button title="Login" />
-      </View>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.topContainer}>
+                <Image
+                    style={styles.image}
+                    source={{ uri: "https://i.ibb.co/BNwMmVn/img.png" }}
+                    resizeMode="contain"
+                />
+                <Text style={styles.text}>DIS STUDENTS</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <CustomButton title="Create a free account" type={ButtonType.PRIMARY} />
+                <CustomButton title="Login" type={ButtonType.SECONDARY} />
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -26,6 +26,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   container: {
+    backgroundColor: "white",
     flex: 1,
     alignItems: "center",
     justifyContent: "space-between",
@@ -38,8 +39,14 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     margin: 10,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    color: "#191259",
   },
   buttonContainer: {
     width: "100%",
+    height: 126,
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
   },
 });
